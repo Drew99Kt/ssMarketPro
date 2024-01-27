97a075e2e3a3652a440380e1313abb24809941d3
andrew <drew99kt@gmail.com>
2024-01-26 21:36:39 -0500

    test

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
 
-  //publicPath: ''
-  publicPath: process.env.NODE_ENV === "production" ? "/app/" : "/", // for github pages
+  publicPath: ''
+  //publicPath: process.env.NODE_ENV === "production" ? "/app/" : "/", // for github pages
 
 })
